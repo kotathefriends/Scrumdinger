aa4e0e11b70930ab413dc8e038df509aa898c2e0
kotathefriends <5070kouta@gmail.com>
2023-12-03 12:25:09 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by KOTA TAKAHASHI on 2023/12/03.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
